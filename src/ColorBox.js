39514797cd1a1ca54c4b4f03e97489e2c89c06cb
swapnil-shukla-np <swapnil.shukla@npglobal.in>
2019-09-03 14:37:04 +0530

    added copied feedback

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+  }
+
+  doCopy = () => {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => {
+        this.setState({ copied: false });
+      }, 1200);
+    });
+  };
   render() {
-      const {name, background} = this.props;
+    const { name, background } = this.props;
     return (
-      <div style={{ background: background }} className="ColorBox">
-        <div className="copy-container">
+      <CopyToClipboard text={background} onCopy={this.doCopy}>
+        <div style={{ background: background }} className="ColorBox">
+          <div
+            style={{ background }}
+            className={this.state.copied ? "copy-overlay show" : "copy-overlay"}
+          ></div>
+          <div className={this.state.copied ? "copy-msg show" : "copy-msg"}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
+          </div>
+          <div className="copy-container">
             <div className="box-content">
-            <span>{name}</span>
+              <span>{name}</span>
             </div>
             <button className="copy-button">Copy</button>
             <span className="see-more">More</span>
+          </div>
         </div>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
