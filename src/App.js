99323fe89276571cacefcba68c03dce8d72c10cb
swapnil-shukla-np <swapnil.shukla@npglobal.in>
2019-09-04 14:19:51 +0530

    added slider;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelper";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[5]} />
+      <Palette palatte={generatePalette(seedColors[5])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ColorBox from "./ColorBox";
-import './Palette.css'
+import "./Palette.css";
+import "rc-slider/assets/index.css";
+import Slider from "rc-slider";
 
 class Palette extends React.Component {
+  state = {
+    level: 500
+  };
+
+  sliderChange = level => {
+    this.setState({ level });
+  };
+
   render() {
-    const colorBoxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} />
+    let { colors } = this.props.palatte;
+    let { level } = this.state;
+
+    const colorBoxes = colors[level].map(color => (
+      <ColorBox key={color.hex} background={color.hex} name={color.name} />
     ));
 
     return (
       <div className="Palette">
         {/* Navbar goes here */}
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onChange={newLevel => {
+            this.sliderChange(newLevel);
+          }}
+        />
 
         <div className="Palette-colors">
           {/* color goes here */}
-
           {colorBoxes}
         </div>
 
